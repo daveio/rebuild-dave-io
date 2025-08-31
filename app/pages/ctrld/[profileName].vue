86e283fa7366467fbb0a11087f7ca833b53c6cea
Dave Williams <dave@dave.io>
2025-08-31 17:30:20 +0100

    regen and bump

---
+++ app/pages/ctrld/[profileName].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>This is the UI for {{ profileName }}.</p>
+  </div>
+</template>
+
+<script
+  setup
+  lang="ts"
+>
+  const profileName = useRoute().params.profileName
+</script>
